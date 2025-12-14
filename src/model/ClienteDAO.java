052a32a9bdf789adf9647335e42da616a05a371c
Pedro aleatório <pedrohenriquecosta2612@gmail.com>
2025-12-14 15:13:11 -0300

    Arquivos Model

--- src/model/ClienteDAO.java
+++ src/model/ClienteDAO.java
@@ -1,5 +1,13 @@
 package model;
 
 public class ClienteDAO {
-    
+
+    public void DadosCliente(Cliente cliente) {
+        System.out.println("Dados do cliente:" + 
+        "Nome: " + cliente.getNome() + 
+        "Email: " + cliente.getEmail());
+    }
+
 }
+
+
