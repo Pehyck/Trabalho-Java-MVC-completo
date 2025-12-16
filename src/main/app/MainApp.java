880c75fd4cc1969292d3d20f839fec53e2375f66
Obunt <silva_daniel28@yahoo.com>
2025-12-16 12:17:07 -0300

    feat: Criação da Main. Criação do Arquivo MainApp.

---
+++ src/main/app/MainApp.java
@@ -0,0 +1,10 @@
+package main.app;
+
+import main.controller.AppController;
+
+public class MainApp {
+    public static void main(String[] args) {
+        AppController controller = new AppController();
+        controller.start();
+    }
+}
